bf36cc7dd3df1a87edd8267d45755b371877cf6e
mkwon3 <mkwon3@students.kennesaw.edu>
2022-11-19 14:17:55 -0500

    Selenium and Chronium
    installed selenium and chronium to scrape the needed webpage

---
+++ SeleniumScrape.py
@@ -0,0 +1,5 @@
+#first need to install selenium and also chronium to use the below code
+from selenium import webdriver
+#give us access to different keys on the key board (like the enter key, escape key, etc...)
+from selenium.webdriver.common.keys import Keys
+import time
